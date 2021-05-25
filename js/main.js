f82f420e8c8412869d91e48805aa3eebe249e3be
Daiana Grimi <grimid.it@gmail.com>
2021-05-25 16:54:01 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,6 @@
-$(document).ready(function () {
-
-    $nav = $('.nav');
-    $toggleCollapse = $('.nav-links');
-
-    /** click event on toggle menu */
-    $toggleCollapse.click(function () {
-        $nav.toggleClass('nav-links');
-    })
-    
-    
-    // owl-carousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:5000,
-
-    });
-
-});
-
-
-<script>
-        var navLinks=document.getElementById("navLinks");
-        function showMenu(){
-            navLinks.style.right="0";
-        }
-        function hideMenu(){
-            navLinks.style.right="-200px";  
-        }
-
-    </script>
+function showMenu(){
+    navLinks.style.right="0";
+}
+function hideMenu(){
+    navLinks.style.right="-200px";  
+}       
