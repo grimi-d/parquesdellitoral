b61c7f05af3f22e948ae52718232f1170e68d176
Daiana Grimi <grimid.it@gmail.com>
2021-06-03 20:05:44 -0300

    main

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ var texto_footer_html = `
 <div class="icons">
 <a target="_blank" href="https://www.facebook.com/parquesdellitoral/"><i class="fab fa-facebook-f"></i></a>
 <a target="_blank" href="https://www.instagram.com/parquesdellitoral/"><i class="fab fa-instagram"></i></a>
-<a target="_blank" href="https://wa.me/5493413227871?text=¡Hola,%20buen%20día!%20Quisiera%20saber%20si%20podrían%20asesorarme%20con%20..."><i class="fab fa-whatsapp"></i></a>
+<a target="_blank" href="https://wa.me/5493416416559?text=¡Hola,%20buen%20día!%20Quisiera%20saber%20si%20podrían%20asesorarme%20con%20..."><i class="fab fa-whatsapp"></i></a>
 <a target="_blank" href="mailto:parquesdellitoral@gmail.com"><i class="fas fa-at"></i></a>
 </div>
 <h4>Desarrollado por Daiana Grimi <i class="far fa-copyright"></i></h4>
